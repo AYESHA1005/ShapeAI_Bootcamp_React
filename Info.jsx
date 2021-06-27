188fa0e7c385f47c74fba2e148c9459bd53e886f
AYESHA SIDDIQUA <82910182+AYESHA1005@users.noreply.github.com>
2021-06-27 15:16:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1> FULL STACK WEB DEVELOPMENT TRAINING AND INTERNSHIP PROGRAM</h1>
+      <p> DSA , CP, REACTJS , NODEJS , TAILWIND CSS , dev-ops etc... </p>
+    </div>
+  );
+}
+
+export default Info;
